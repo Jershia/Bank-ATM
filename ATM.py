c4d827a3ae4cbbc55f2a6af1c4e232fcb75d09d1
Jershia <74312429+Jershia@users.noreply.github.com>
2021-07-31 15:42:16 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,29 @@
+class ATM(object):
+    def __init__(self,number,pin):
+        self.number = number
+        self.pin = pin
+        
+    def CashWithdrawl (self,takenAmount):
+        amount = 85990 - takenAmount
+        print("Your Balance Amount is " + str(amount))
+
+    def BalanceEnquiry(self):
+        print("Your Balance Amount is 85990")
+
+def main():
+        card = input("Insert your card number :")
+        pin = input("Enter your pin :")
+        user = ATM(card,pin)
+        print("Enter 'b' for Balance enquiry and 'c' for Cash Withdrawl")
+        choose = input("Enter your choice :")
+        if(choose == "b"):
+            user.BalanceEnquiry()
+        elif(choose == "c"):
+            takenAmount = int(input("Enter the amount :"))
+            user.CashWithdrawl(takenAmount)
+        else:
+            print("Not Valid")
+
+name = 0
+if(name == 0):
+    main()
